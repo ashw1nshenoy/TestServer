35f00cdf1d4c96ec2bb7b5d2a59d058ed45859ee
Ashwin Shenoy <ashwinshenoy20@gmail.com>
2025-05-09 13:45:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 
 app.post("/", (req, res) => {
-    const data = JSON.stringify(req.body, null, 2); // Format the JSON for readability
-    const filePath = "systeminfo.json"; // You can change the file name/path as needed
-
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            console.error("Error writing to file:", err);
-            return res.status(500).send("Failed to save data.");
-        }
-        res.send("Data saved successfully.");
-    });
+    console.log(req.body)
 });
 
 app.listen(3000, "0.0.0.0", () => {
